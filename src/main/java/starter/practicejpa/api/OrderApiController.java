9a4a2794f51c524fc12cc789951c43415e18e19a
U-jjin <U-jjin@githum.com>
2022-11-29 20:13:50 +0900

    [feat] JPA DTO 직접 조회 기능 구현

---
+++ src/main/java/starter/practicejpa/api/OrderApiController.java
@@ -0,0 +1,134 @@
+package starter.practicejpa.api;
+
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import starter.practicejpa.domain.Address;
+import starter.practicejpa.domain.Order;
+import starter.practicejpa.domain.OrderItem;
+import starter.practicejpa.domain.OrderStatus;
+import starter.practicejpa.repository.OrderRepository;
+import starter.practicejpa.repository.OrderSearch;
+import starter.practicejpa.repository.order.OrderQueryDto;
+import starter.practicejpa.repository.order.OrderQueryRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+
+    private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
+
+    /*
+
+        V1. 엔티티 직접 노출
+        - 엔티티가 변하면 API 스펙이 변한다.
+        - 트랜잭션 안에서 지연 로딩 필요
+        - Hibernate5Module 모듈 등록, LAZY=null 처리
+        - 양방햔 연관관계 문제 ->@JsonIgnore
+
+     */
+
+    @GetMapping("api/v1/orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        //iter 치고 tab하면 자동으로 바꿔준다.
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o->o.getItem().getName());
+
+        }
+        return all;
+    }
+
+    @GetMapping("api/v2/orders")
+    public List<OrderDto> ordersV2(){
+        /* V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+         * - 트랜잭션 안에서 지연 로딩 필요
+
+         */
+        List<Order> orders = orderRepository.findAll(new OrderSearch());
+        List<OrderDto> collect = orders
+                                .stream()
+                                .map(o->new OrderDto(o))
+                                .collect(Collectors.toList());
+        return collect;
+    }
+
+    @GetMapping("api/v3/orders")
+    public List<OrderDto> ordersV3(){
+        List<Order> orders =orderRepository.findAllWithItem();
+        List<OrderDto> result = orders.stream()
+                .map(o-> new OrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @GetMapping("api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(@RequestParam(value = "offset",defaultValue = "0")int offset,
+                                        @RequestParam(value = "limit", defaultValue = "100") int limit){
+        //ToOne 관계는 그냥 페치조인으로 가져온다.
+        List<Order> orders =orderRepository.findAllWithMemberDelivery(offset,limit);
+
+
+        List<OrderDto> result = orders.stream()
+                .map(o-> new OrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @GetMapping("api/v4/orders")
+    public List<OrderQueryDto> ordersV4(){
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
+
+    @Data
+    static class OrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDateTime;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDateTime = order.getOrderDateTime();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems()
+                    .stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Data
+    static public class OrderItemDto{
+
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem){
+            this.itemName = orderItem.getItem().getName();
+            this.orderPrice = orderItem.getOrderPrice();
+            this.count = orderItem.getCount();
+        }
+    }
+}
+
+
+
---
+++ src/main/java/starter/practicejpa/repository/order/OrderQueryDto.java
@@ -0,0 +1,31 @@
+package starter.practicejpa.repository.order;
+
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import starter.practicejpa.domain.Address;
+import starter.practicejpa.domain.OrderStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(of="orderId")
+public class OrderQueryDto {
+
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDateTime;
+    private OrderStatus orderStatus;
+    private Address address;
+    private List<OrderItemQueryDto> orderItems;
+
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDateTime, OrderStatus orderStatus,Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderStatus = orderStatus;
+        this.orderDateTime = orderDateTime;
+        this.address = address;
+    }
+}
